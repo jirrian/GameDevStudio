43e10ef381ad2961c27b92a3ef82fbfd4e941841
Jillian Zhong <jillianzhong@yahoo.com>
2015-07-22 17:48:44 -0400

    Started midterm project, added rough blocking of scene

---
+++ Week3A/Demo/Assets/scripts/MoveRigidbody.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveRigidbody : MonoBehaviour {
+
+	public float speed = 5f;
+	public float turnSpeed = 90f;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float x = Input.GetAxis("Horizontal");
+		float y = Input.GetAxis("Vertical");
+
+		Rigidbody rbody = GetComponent<Rigidbody>();
+
+		//rotating
+		//rbody.AddRelativeForce(0f, 0f, y * speed * Time.deltaTime);
+
+		//transform.Rotate(0f, x * turnSpeed * Time.deltaTime, 0f);
+
+		//side stepping
+		rbody.AddRelativeForce(x * speed * Time.deltaTime, 0f, y * speed * Time.deltaTime);
+	}
+}
---
+++ Week3A/Demo/Assets/scripts/deathCubeTrigger.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class deathCubeTrigger : MonoBehaviour {
+	public ParticleSystem fire;
+
+	// delete player if enter trigger
+	void OnTriggerEnter(Collider thing){
+		Destroy(thing.gameObject);	// delete game object
+
+		fire.Play(); // start particle system
+	}
+}
